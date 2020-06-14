b4a0c1f2c09bb8c705f7859047e56529c48122e6
Noel De Martin <noeldemartin@gmail.com>
2020-06-14 10:27:39 +0200

    Improve status command

--- scripts/status.sh
+++ scripts/status.sh
@@ -3,7 +3,20 @@
 if [[ -z `docker container ls --all --quiet --filter name=nginx-agora` ]]; then
     echo "'nginx-agora' container is not created"
 elif [[ -z `docker container ls --quiet --filter name=nginx-agora` ]]; then
-    echo "'nginx-agora' container is not running"
+    printf "\033[0;31m'nginx-agora' container is not running\033[0m\n"
 else
-    echo "'nginx-agora' container is running"
+    printf "\033[0;32m'nginx-agora' container is running\033[0m\n"
 fi
+
+echo ""
+
+for name in `ls $base_dir/sites_installed`; do
+    siteroot=`sed -n "1p" $base_dir/sites_installed/$name`
+    siteconfig=`sed -n "2p" $base_dir/sites_installed/$name`
+
+    if [[ -f "$base_dir/sites_enabled/$siteconfig" ]]; then
+        printf "\033[0;32m[enabled]\033[0m  $name [$siteroot]\n"
+    else
+        printf "\033[0;31m[disabled]\033[0m $name [$siteroot]\n"
+    fi
+done
