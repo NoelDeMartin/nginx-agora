bc1538add10d4316cb90c16c456c4ce17edf0a30
Noel De Martin <noel@noeldemartin.com>
2025-05-26 18:33:42 +0200

    Redirect stderr in crontab config

--- scripts/cron/renew-certificates.sh
+++ scripts/cron/renew-certificates.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # sudo crontab -e
-# 0 0 * * 1 /var/www/nginx-agora/scripts/cron/renew-certificates.sh >> /var/log/cron-certbot.log
+# 0 0 * * 1 /var/www/nginx-agora/scripts/cron/renew-certificates.sh >> /var/log/cron-certbot.log 2>> /var/log/cron-certbot.log
 
 echo "[`date`] Renewing certificates..."
 certbot renew --pre-hook "nginx-agora stop" --post-hook "nginx-agora start"
